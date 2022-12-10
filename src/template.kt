e628270fee9a5b8ff11f84fbe3fceb0ac430327e
jiajianchen <jiajianchen@tencent.com>
2022-12-10 14:59:56 +0800

    template: add template

---
+++ src/template.kt
@@ -0,0 +1,13 @@
+fun main() {
+    fun part1(input: List<String>): Int = 0
+
+    fun part2(input: List<String>): Int = 0
+
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 0)
+//    check(part2(testInput) == 0)
+
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
